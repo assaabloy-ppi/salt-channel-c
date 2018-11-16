22ff13f18f190b41e323da1bf81fa274f4e947a6
Simon Johansson <simon.johansson@assaabloy.com>
2018-11-16 13:36:02 +0100

    Remove printout

--- examples/salt_io.c
+++ examples/salt_io.c
@@ -25,8 +25,6 @@ salt_ret_t my_write(salt_io_channel_t *p_wchannel)
         return SALT_ERROR;
     }
 
-    printf("p_rchannel->p_data: 0x%p\r\n", (void *) p_wchannel->p_data);
-
     int n = write(sock,
                   &p_wchannel->p_data[p_wchannel->size],
                   to_write);
